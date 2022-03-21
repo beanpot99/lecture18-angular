71c2714c49765620af92ddc9c4d079a7b94975b4
Grace Thompson <Grace.thompson17@aol.com>
2022-03-21 18:54:39 -0500

    class activity

---
+++ src/app/labeled-input/labeled-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-labeled-input',
+  templateUrl: './labeled-input.component.html',
+  styleUrls: ['./labeled-input.component.css']
+})
+export class LabeledInputComponent implements OnInit {
+  myTagName: string='';
+  constructor(elm:ElementRef) { 
+    this.myTagName=elm.nativeElement.getAttribute('data-label');
+  }
+
+  ngOnInit() {
+  }
+
+}
